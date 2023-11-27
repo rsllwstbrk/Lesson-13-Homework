62bc4096fff7cd2fd1d78f1edc392ddfaf981f7e
Russell <rshtyrev@gmail.com>
2023-11-27 19:18:15 +0300

    view added through storyboard

--- Lesson 13 Homework/ThirdViewController.swift
+++ Lesson 13 Homework/ThirdViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 class ThirdViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var tableView: UITableView!
-    let middleView = UIView()
-    let notesTextField = UITextField()
     
 
     override func viewDidLoad() {
@@ -27,25 +25,6 @@ class ThirdViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        tableView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        tableView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
-        cell.textLabel?.text = ""
-        cell.addSubview(middleView)
-        cell.heightAnchor.constraint(equalToConstant: 550).isActive = true
-        middleView.centerXAnchor.constraint(equalTo: cell.centerXAnchor).isActive = true
-        middleView.centerYAnchor.constraint(equalTo: cell.centerYAnchor).isActive = true
-        middleView.translatesAutoresizingMaskIntoConstraints = false
-        middleView.widthAnchor.constraint(equalToConstant: 369).isActive = true
-        middleView.heightAnchor.constraint(equalToConstant: 550).isActive = true
-        middleView.backgroundColor = .gray
-        middleView.addSubview(notesTextField)
-        notesTextField.text = "Notes:"
-        notesTextField.translatesAutoresizingMaskIntoConstraints = false
-        notesTextField.centerXAnchor.constraint(equalTo: middleView.centerXAnchor, constant: -150).isActive = true
-        notesTextField.centerYAnchor.constraint(equalTo: middleView.centerYAnchor, constant: -257).isActive = true
         return cell
     }
 
